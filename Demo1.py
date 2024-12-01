d96c3276823e440f7bceb5ea787987da2bddce6d
pramod <shejul.pramod22@gmail.com>
2024-12-01 07:15:02 -0700

    changes moving to Local Repository

---
+++ Demo1.py
@@ -0,0 +1,81 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+a = 10;
+type(a)
+<class 'int'>
+b=20.23
+type(b)
+<class 'float'>
+c=0000
+type(c)
+<class 'int'>
+d="Pramod shejul"
+type(d)
+<class 'str'>
+a,b,c = 100,20.55, "Pramod"
+a
+100
+b
+20.55
+c
+'Pramod'
+a=b=c=500
+a
+500
+b
+500
+c
+500
+>>> a_shsjdkdjsks=5000
+>>> a
+500
+>>> 1a_=1000
+SyntaxError: invalid decimal literal
+>>> 
+>>> import keyword
+>>> keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> 
+>>> #Demo 4 day
+>>> a=100
+>>> a=11161616626262635353644748833992922003040403022
+>>> b=20.36372327267367373737373773737373737733773737373777337337
+>>> b
+20.363723272673674
+>>> a=100
+>>> a=200
+>>> a
+200
+>>> c=2+3j
+>>> type(c)
+<class 'complex'>
+>>> 
+>>> s='PRAMOD'
+>>> s="PRAMOD"
+>>> s='''PRAMOD'''
+>>> s
+'PRAMOD'
+>>> 
+>>> s='RAM'
+>>> s="Ram"
+>>> s='''ram'''
+>>> s
+'ram'
+>>> b=10.2323232323787878787
+>>> b
+10.232323232378787
+>>> 
+>>> #Boolean Literals
+>>> a=True
+>>> a
+True
+>>> b=False
+>>> b
+False
+>>>
+>>> #Special Literals
+>>> a=None
+
+
+
